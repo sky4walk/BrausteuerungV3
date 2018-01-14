752ca4d5bb602fd6fb43afd131e130025c1234c9
Andre Betz <git@andrebetz.de>
2018-01-14 18:42:06 +0100

    changed port

--- BackEnd/Arduino/installFW.bat
+++ BackEnd/Arduino/installFW.bat
@@ -3,6 +3,6 @@ REM %AVRPATH%\listComPorts.exe aufrufen um COM Port zu bestimmen
 REM erhaltenen COM Port anstatt COM7 eintragen
 
 SET AVRPATH=avrdude
-SET COMPORT=COM5
+SET COMPORT=COM3
 %AVRPATH%\avrdude.exe -C%AVRPATH%\avrdude.conf -v -patmega328p -carduino -P%COMPORT% -b57600 -D -Uflash:w:Software\bin\Brausteuerung_301709.hex:i 
 REM %AVRPATH%\avrdude.exe -C%AVRPATH%\avrdude.conf -v -patmega328p -carduino -P%COMPORT% -b57600 -D -Uflash:w:Software\bin\Brausteuerung_ProfiCook.hex:i
