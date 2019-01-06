64f8a06f8811510d215e4abad96368f3c66cc14d
Andre Betz <git@andrebetz.de>
2019-01-06 14:33:49 +0100

    new 433MHz codes

--- BackEnd/Arduino/installFW.bat
+++ BackEnd/Arduino/installFW.bat
@@ -3,7 +3,7 @@ REM %AVRPATH%\listComPorts.exe aufrufen um COM Port zu bestimmen
 REM erhaltenen COM Port anstatt COM7 eintragen
 
 SET AVRPATH=avrdude
-SET COMPORT=COM3
+SET COMPORT=COM8
 
 REM install Bluetooth
 %AVRPATH%\avrdude.exe -C%AVRPATH%\avrdude.conf -v -patmega328p -carduino -P%COMPORT% -b57600 -D -Uflash:w:HC06-Term\HC06-Term.cpp.hex:i 
